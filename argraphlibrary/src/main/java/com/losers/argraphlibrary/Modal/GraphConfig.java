8887384b08fa0219b87e92768875836ce1b3ad94
deepak <deepak4u93@gmail.com>
2019-02-04 22:06:32 +0530

    added demo project

--- argraphlibrary/src/main/java/com/losers/argraphlibrary/Modal/GraphConfig.java
+++ argraphlibrary/src/main/java/com/losers/argraphlibrary/Modal/GraphConfig.java
@@ -75,30 +75,53 @@ public class GraphConfig {
 
     private List<Double> GraphList = new ArrayList<>();
     private boolean isLoggingEnable = false;
-    private boolean isEnableClassicPlatform = true;
+    private boolean isEnableClassicPlatform = false;
     private boolean isEnableVideo = false;
     private VIDEO_QUALITY mVIDEO_quality = VIDEO_QUALITY.QUALITY_720P;
 
+    /**
+     * @param enableLogging true to enable logs, false to disable
+     */
     public Builder setEnableLogging(@NonNull boolean enableLogging) {
       isLoggingEnable = enableLogging;
       return this;
     }
+
+    /**
+     * set video quality of video recording
+     *
+     * @param videoQuality QUALITY_HIGH, QUALITY_2160P, QUALITY_1080P, QUALITY_720P, QUALITY_480P
+     */
 
     public Builder setVideoQuality(@NonNull VIDEO_QUALITY videoQuality) {
       mVIDEO_quality = videoQuality;
       return this;
     }
 
+    /**
+     * if you want to allow user to take video recording of graph
+     *
+     * @param enableVideo true to enable , bydefault it is off
+     */
     public Builder setEnableVideo(@NonNull boolean enableVideo) {
       isEnableVideo = enableVideo;
       return this;
     }
 
+
+    /**
+     * if you want to show graph on platform
+     *
+     * @param enableClassicPlatform true to enable , bydefault it is off
+     */
     public Builder setEnableClassicPlatform(@NonNull boolean enableClassicPlatform) {
       isEnableClassicPlatform = enableClassicPlatform;
       return this;
     }
 
+    /**
+     * List of point you want to show on a graph
+     */
     @NonNull
     public Builder setGraphList(@NonNull List<Double> graphList) {
       this.GraphList = graphList;
